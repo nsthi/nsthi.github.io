45b9b744bfba55d849a74a86543f78009f9e3f1a
Neil Sethi <neilsethi@gmail.com>
2015-01-13 22:45:39 -0800

    Revert "Refactored Javascript to allow Fluidbox to work with previously hidden elements. Optimized CSS for Safari."
    This reverts commit 4a96e4d89bb5f1c61cc00f4f7d3f3e5c3a5bf07b.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,53 @@
 $(document).ready(function(){
+$(window).load(function() {
 
     // show proper page on load
-    var hash = location.hash.replace('#', '');
+    var hash = location.hash.replace('#','');
     if (hash != "")
     {
-				if (hash == 'photo'){
-					$("#main > div").hide();
-	        $("#" + hash + "_section").fadeIn({duration:500}).find('#portfolio a').fluidbox();
-	        
-				}
-				else{
-					$("#main > div").hide();
-	        $("#" + hash + "_section").fadeIn({duration:500});
-				}
+        $("#main > div").hide();
+        $("#" + hash + "_section").fadeIn({duration:0});
     }
         
     // page transitions
     $("#title a, #navigation a").click(function(e) {
         $("#main > div").hide();
         target = $(this).attr("href") + "_section";
-        if (target == "#photo_section")
-				{
-	        $(target).fadeIn({duration:500}).find('#portfolio a').fluidbox();
-				}
-				else {
-					$(target).fadeIn({duration: 500});					
-				}
+        $(target).fadeIn({duration: 500});
     });
+
+    // // logging
+    // var request = $.get('/path/to/resource.ext');
+
+    // request.success(function(result) {
+    //   console.log(result);
+    // });
+
+    // request.error(function(jqXHR, textStatus, errorThrown) {
+    //   if (textStatus == 'timeout')
+    //     console.log('The server is not responding');
+
+    //   if (textStatus == 'error')
+    //     console.log(errorThrown);
+
+    //   // Etc
+    // });
+
+    // light box
+
+    if ($("#photo_section").is(":visible")) 
+    {
+            $('#portfolio a').fluidbox({
+            viewportFill: 1.0,
+            closeTrigger: [
+            { selector: '#portfolio a', event: 'click'  },
+            { selector: 'window', event: 'resize' }
+        ],
+    });
+    }
+
+
+
+});
 
 });

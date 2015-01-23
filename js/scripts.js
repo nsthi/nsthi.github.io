9da1fbb518ee1e35e957e3269982b73681cfc4b3
Neil Sethi <neilsethi@gmail.com>
2015-01-22 23:00:37 -0800

    Full commit of new website, still need to do asset cleanup

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+    // // show proper page on load
+    // var hash = location.hash.replace('#', '');
+    // if (hash != "")
+    // {
+				// if (hash == 'photo'){
+				// 	$("#main > div").hide();
+	   //      $("#" + hash + "_section").fadeIn({duration:500}).find('#portfolio a').fluidbox();
+	        
+				// }
+				// else{
+				// 	$("#main > div").hide();
+	   //      $("#" + hash + "_section").fadeIn({duration:500});
+				// }
+    // }
+        
+    // // page transitions
+    // $("#title a, #navigation a").click(function(e) {
+    //     $("#main > div").hide();
+    //     target = $(this).attr("href") + "_section";
+    //     if (target == "#photo_section")
+				// {
+	   //      $(target).fadeIn({duration:500}).find('#portfolio a').fluidbox();
+				// }
+				// else {
+				// 	$(target).fadeIn({duration: 500});					
+				// }
+    // });
+	$('#portfolio a').fluidbox();
+
+});
